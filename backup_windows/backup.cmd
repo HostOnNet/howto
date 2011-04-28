39cb00f6da914b75630fe0a6753ac734bbbd0d84
HostOnNet.com <santhosh@hostonnet.com>
2011-04-28 10:11:16 +0530

    backup firefox movies to os backup
    vshare requirements upgraded softwares jpegsrc.v8c.tar.gz and yamdi-1.8.tar.gz
    
    Added Mplayer install from tar.gz (optional)

--- backup_windows/backup.cmd
+++ backup_windows/backup.cmd
@@ -1,6 +1,5 @@
 # Backup runes daily at 10 AM
 
-robocopy.exe D:\os\firefox_profile\ I:\backup_drive_d\os\firefox_profile\ /R:1 /MIR
 robocopy.exe D:\ I:\backup_drive_d /R:2 /XD "D:\System Volume Information" d:\os d:\RECYCLER D:\$RECYCLE.BIN /MIR
 robocopy.exe C:\Windows\System32\drivers\etc\ I:\backup_others\etc\ /R:2 /MIR
 REGEDIT.EXE /E D:\programs\putty_backup.reg "HKEY_CURRENT_USER\Software\SimonTatham"
--- backup_windows/backup_os.cmd
+++ backup_windows/backup_os.cmd
@@ -1,3 +1,2 @@
-
-robocopy.exe d:\os\ I:\backup_drive_d\os\ /XD D:\os\firefox_profile\ /MIR
+robocopy.exe d:\os\ I:\backup_drive_d\os\ /MIR
 echo %date% - %time% - OS Backup Complete >> D:\backup_log.txt
